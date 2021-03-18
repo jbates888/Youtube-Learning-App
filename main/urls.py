f324d2e99b6c307af213bd14b9c783818cc72fa5
thutaj4 <thutaj4@gmail.com>
2021-03-17 19:36:00 -0500

    Updates: - two drop down menus that populate after a search - user is able to select one or more videos, and select a list they want to add the videos to - if either a list is not selected or a video, page will refresh to the home page

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
 path("<int:id>", views.index, name="index"),
 path("", views.home, name="home"),
-path("home/", views.home, name="home"),
+path("home/", views.home, name="home2"),
 path("create/", views.create, name="create"),
 path("view/", views.view, name="view"),  # <-- added
+path("addVideo/", views.addVideo, name="addVideo") # <-- added by aj
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, response
+from django.shortcuts import redirect
 from django.utils import timezone
-from .models import ToDoList
+from .models import ToDoList, CheckBox
 from .forms import CreateListForm
 import re
 from pytube import YouTube
@@ -39,6 +40,22 @@ def index(response, id):
         return render(response, "main/index.html", {"ls": ls})
     return render(response, "main/home.html", {})
 
+def addVideo(response):
+    #get the list of selected video links and selected list
+    videoLinks = response.GET.getlist("Checked-Video")
+    listName = response.GET.get("Checked-List")
+
+    #if either aren't selected then refresh the home page
+    if (listName is None) or len(videoLinks) == 0:
+        return redirect('home')
+    else:
+        ls = ToDoList.objects.get(name = listName)
+        #add all video links to the list and take user to view the list
+        for video in videoLinks:
+            ls.item_set.create(text=video, complete=False)
+        return render(response, "main/index.html", {"ls": ls})
+
+
 def create(response):
     if response.method == "POST":
         form = CreateListForm(response.POST)
@@ -58,9 +75,16 @@ def create(response):
 def home(request):
     search_url = 'https://www.googleapis.com/youtube/v3/search'
     video_url = 'https://www.googleapis.com/youtube/v3/videos'
+    #create two arrays of boxes, one for the videos and one for the lists, array of indexes
+    vidBoxes, listBoxes, indexs = [], [], []
+    for i in range(9):
+        vidBoxes.append(CheckBox())
+        listBoxes.append(CheckBox())
+        indexs.append(i)
 
     userSearch = request.GET.get('search', '')
     videos = []
+    vidCopy = []
     if userSearch != '':
         url = spaceReplace(userSearch)
         html = urllib.request.urlopen(url)
@@ -68,7 +92,8 @@ def home(request):
         for i in range(9):
             fullLink = "https://www.youtube.com/embed/" + videoIDS[i]
             videos.append(fullLink)
-    return render(request, "main/home.html", {"videos": videos})
+            vidCopy.append(fullLink)
+    return render(request, "main/home.html", {"videos": zip(videos, vidBoxes, listBoxes, indexs), "copy": zip(vidCopy, indexs)})
 
 
 def view(response):
