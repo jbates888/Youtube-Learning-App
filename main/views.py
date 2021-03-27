15f7bd949003a9a73cae9dc006e44eb0980ad08f
thutaj4 <thutaj4@gmail.com>
2021-03-27 10:45:49 -0500

    fixed website links issue

--- main/views.py
+++ main/views.py
@@ -7,13 +7,14 @@ from .forms import CreateListForm
 import re
 from pytube import YouTube
 import urllib.request
-from googlesearch import search
+# from googlesearch import search
 import requests
 from django.conf import settings
 from isodate import parse_duration
 from django.contrib import messages
 from bs4 import BeautifulSoup
 import validators
+from requests import get
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
@@ -64,7 +65,7 @@ def addVideo(response):
             ls.item_set.create(text=title, complete=False, video=False, siteLink=link, website=True)
 
         #take user to view the current list
-        return render(response, "main/view.html", {"ls": ls,'message': True})
+        return render(response, "main/view.html", {"ls": ls, 'message': True})
 
 def create(response):
     if response.method == "POST":
@@ -140,3 +141,30 @@ def spaceReplace(search):
     search = re.sub(r"\s+", '+', search)
     temp = "https://www.youtube.com/results?search_query=" + "learn" + search
     return temp
+
+def search(term, num_results=10, lang="en"):
+    usr_agent = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/61.0.3163.100 Safari/537.36'}
+
+    def fetch_results(search_term, number_results, language_code):
+        escaped_search_term = search_term.replace(' ', '+')
+
+        google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_search_term, number_results+1,
+                                                                              language_code)
+        response = get(google_url, headers=usr_agent)
+        response.raise_for_status()
+
+        return response.text
+
+    def parse_results(raw_html):
+        soup = BeautifulSoup(raw_html, 'html.parser')
+        result_block = soup.find_all('div', attrs={'class': 'g'})
+        for result in result_block:
+            link = result.find('a', href=True)
+            title = result.find('h3')
+            if link and title:
+                yield (link['href'], title)
+
+    html = fetch_results(term, num_results, lang)
+    return list(parse_results(html))
