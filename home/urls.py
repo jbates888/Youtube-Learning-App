01c9cb86b0cc77549fb1bc85360b36b1f82d3935
Jack Bates <41972596+jbates888@users.noreply.github.com>
2021-05-02 19:28:17 -0500

    Delete home directory

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<str:name>', views.home, name='app-home'),
-    #path('', views.home, name = 'app-home'),
-
-    #  path('account/', views.account, name='app-account'),
-]
